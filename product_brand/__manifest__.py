38e267f38b6b00a8a1b5ad8bb8ef3f83bcf3613c
shyamg <shyamg@emiprotechnologies.com>
2022-03-22 15:11:48 +0530

    Initial Commit

---
+++ product_brand/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Brand',
+    'summary': 'Product Brand',
+    'description': """
+        Odoo Ecommerce Exercise 2
+    """,
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
